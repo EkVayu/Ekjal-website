c629961cf41aeb2afb58c7a410e0c0474d6286d9
Siddharth Bhatia <siddharthbhatia40@gmail.com>
2024-11-06 18:38:15 +0530

    fooetr update

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
-import { Instagram, Twitter, Linkedin, Github, Phone, Mail, MapPin } from "lucide-react";
+import {
+  Instagram,
+  Twitter,
+  Linkedin,
+  Github,
+  Phone,
+  Mail,
+  MapPin,
+} from "lucide-react";
 import logo from "../assets/logo.png";
 
 const footerSections = [
@@ -56,32 +64,36 @@ function Footer() {
               <div className="mb-4 lg:-mt-2">
                 <Link to="/" className="flex items-center space-x-2">
                   <img src={logo} className="h-8 rounded-sm" alt="Ekjal logo" />
-                  <span className="text-2xl font-semibold">Ekjal</span>
+                  <span className="text-2xl font-semibold text-primary">
+                    Ekjal
+                  </span>
                 </Link>
               </div>
               <p className="text-gray-500 mb-4">
-                Bridging the cybersecurity skills gap, fostering innovation, and enhancing resilience are at the forefront of our strategic initiatives at Ekjal
+              Bridging the cybersecurity skills gap, fostering innovation, and enhancing resilience are at the forefront of our strategic initiatives at Ekjal
               </p>
               <div className="flex flex-col space-y-2 text-gray-500">
                 <div className="flex items-center">
-                  <Phone className="h-5 w-5 mr-2" />
+                  <Phone className="h-5 w-5 mr-2 text-primary" />
                   <span>+91 98731 34795</span>
                 </div>
                 <div className="flex items-center">
-                  <Mail className="h-5 w-5 mr-2" />
+                  <Mail className="h-5 w-5 mr-2 text-primary" />
                   <span>pravin@ekjal.com</span>
                 </div>
                 <div className="flex items-start">
-                  <MapPin className="h-5 w-5 mr-2 mt-1" />
-                  <span>JSS Science & Tech Entrepreneurs Park (STEP) C-20/I, Sector 62, NOIDA - 201301, UP</span>
+                  <MapPin className="h-7 w-7 mr-2 mt-1 text-primary" />
+                  <span>
+                    JSS Science & Tech Entrepreneurs Park (STEP) C-20/I, Sector
+                    62, NOIDA - 201301, UP
+                  </span>
                 </div>
               </div>
             </div>
-          
 
             {footerSections.map((section, index) => (
               <div key={index}>
-                <div className="mb-4 font-bold uppercase tracking-widest text-gray-800">
+                <div className="mb-4 font-bold uppercase tracking-widest text-primary">
                   {section.title}
                 </div>
                 <nav className="flex flex-col gap-4">
@@ -89,7 +101,7 @@ function Footer() {
                     <div key={linkIndex}>
                       <Link
                         to={link.to}
-                        className="text-foreground/50 transition duration-100 hover:text-primary active:text-primary"
+                        className="text-foreground/50 transition duration-100 hover:text-primary hover:font-bold active:text-primary"
                       >
                         {link.title}
                       </Link>
@@ -103,9 +115,9 @@ function Footer() {
       </div>
 
       <div className="bg-secondary border-t border-foreground/20">
-        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="px-3 lg:px-20">
           <div className="flex items-center justify-between gap-4 py-8">
-            <span className="text-sm text-gray-400">
+            <span className="text-base text-primary font-bold">
               © 2024 - Present Ekjal. All rights reserved.
             </span>
 
@@ -116,7 +128,7 @@ function Footer() {
                   to={href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-gray-400 transition duration-100 hover:text-primary active:text-primary"
+                  className="text-primary transition duration-100 hover:text-primary/80"
                 >
                   <Icon className="h-5 w-5" />
                 </Link>
